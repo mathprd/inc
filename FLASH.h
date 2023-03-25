0e3bea4b33b5a12e56b1b3df6667bd459fad9c3f
mathprd <119405317+mathprd@users.noreply.github.com>
2023-03-25 21:12:54 +0100

    fin samedi

---
+++ FLASH.h
@@ -0,0 +1,16 @@
+/*
+ * FLASH.h
+ *
+ *  Created on: 25 mars 2023
+ *      Author: mathi
+ */
+
+#ifndef FLASH_H_
+#define FLASH_H_
+
+
+
+
+
+
+#endif /* FLASH_H_ */
--- Service_Timer.h
+++ Service_Timer.h
@@ -11,8 +11,9 @@
 void Init_TIM2_Tempo(void);
 void Stop_Tempo(void);
 void Run_Tempo(void);
-void Init_TIM3_Pompe_1mn(void);
+void Init_TIM3_Pompe_1sec(void);
 void TIM3_IRQHandler(void);
+void TIM2_IRQHandler(void);
 void Run_Pompe_1sec(void);
 void Stop_Pompe_1sec(void);
 
